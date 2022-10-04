dd1151c42e6050824da95e9e5ca43e42a8a1e4e3
AlenaD-B27 <lenador.18@gmail.com>
2022-10-04 00:16:16 -0500

    changes during group practice: 10/3/2022. Next question is number 26

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
-
+import java.util.Scanner;
 
 public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String daysOfTheWeek [] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    }
 
 }
